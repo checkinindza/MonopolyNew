7f2f9eedba36d9d12cde6bff6bb2a3def6b7581e
checkinindza <tomasfilipavicius13@gmail.com>
2024-03-11 22:15:01 +0200

    Added a few tests cases for DataHandler class

--- src/test/java/testDataHandler.java
+++ src/test/java/testDataHandler.java
@@ -1,8 +1,38 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import java.util.Deque;
+
 class testDataHandler {
+
+    DataHandler data;
+
+    @BeforeEach
+    void setUp () {
+        data = new DataHandler();
+    }
+
     @Test
+    @DisplayName ("Testing if data was loaded succesfully")
     void dataIsLoadedSuccesfully() {
-        DataHandler data = new DataHandler();
-        Boolean loaded = data.isDataLoaded();
-        assertTrue(loaded == true, () -> "Data was not loaded");
+        assertEquals(10, data.getCardsCollectionSize(), "The collection size should be 10");
+    }
+
+    @Test
+    @DisplayName ("Test if players deque is being set up properly")
+    void testIfPlayerWasCreated() {
+        int initialMoney = 100;
+        int initialPoints = 50;
+        int numberOfPlayers = 3;
+
+        data.setupPlayers(initialMoney, initialPoints, numberOfPlayers);
+        Deque<DataHandler.Player> playersDeque = data.getPlayerDeque();
+        assertEquals(numberOfPlayers, playersDeque.size(), "Size of deque should be 3");
+
+        for (DataHandler.Player player : playersDeque) {
+            assertEquals(initialMoney, player.getMoney());
+            assertEquals(initialPoints, player.getPoints());
+        }
     }
 }
